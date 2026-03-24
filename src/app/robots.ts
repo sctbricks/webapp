6ae40196efcf0e833600975d89fc15de52ad5f07
sctbricks <sctbricks@gmail.com>
2026-03-24 09:45:25 +0530

    Add files via upload

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/private/'],
+      },
+    ],
     host: 'https://sctbricks.com',
     sitemap: 'https://sctbricks.com/sitemap.xml',
 
